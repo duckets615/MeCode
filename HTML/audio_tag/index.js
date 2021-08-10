5b8af1e90679a443732219979466a5016e1719e6
ChrisReilly024 <chris.reilly024@gmail.com>
2021-08-09 20:20:21 -0400

    user display + controls completed

---
+++ HTML/audio_tag/index.js
@@ -0,0 +1,42 @@
+const playlist = ['bright', 'on', 'tink', 'snare', 'kick'];
+const songs = {
+    bright: 'Mr. Brightside - The Killers',
+    on: 'On & On & On - Catch 22',
+    tink: 'Symbol Tink',
+    snare: 'Drum Snare',
+    kick: 'Drum Kick'    
+}
+let current = 0; 
+let currentSong = songs[playlist[current]];
+
+const playButton = document.querySelector('.play');
+const pauseButton = document.querySelector('.pause');
+const stopButton = document.querySelector('.stop');
+const song = document.querySelector(`audio`);
+const box = document.querySelectorAll('.box');
+const arrow = document.querySelectorAll('.arrow');
+const songTitle = document.querySelector('.songTitle')
+
+playButton.addEventListener('click', (e) => doStuff(e));
+pauseButton.addEventListener('click', (e) => doStuff(e));
+stopButton.addEventListener('click', (e) => doStuff(e));
+arrow.forEach(arrow => arrow.addEventListener('click', (e) => nextSong(e)));
+
+function doStuff(e) {
+    const song = document.querySelector(`audio[data-key="${playlist[current]}"`);
+    switch(e.target.textContent) {
+        case('Start'): song.play();
+        case('Pause'): song.pause();
+        default: song.currentTime = 4000
+    }    
+    box.forEach(box => box.classList.remove('selected', 'selectedStop'));
+    e.target.textContent === 'Stop' ? e.target.classList.add('selectedStop') : e.target.classList.add('selected');
+} 
+function nextSong(e) {
+    if (e.target.textContext === '>') {
+        current === Object.keys(songs).length - 1 ? current = 0 : current++;
+    } else {
+        current === 0 ? current = Object.keys(songs).length - 1 : current--;
+    }    
+    songTitle.textContent = `${songs[playlist[current]]}`
+}
